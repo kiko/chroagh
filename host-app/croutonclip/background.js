b0bc9c582eb822227ca9e805c6df78613333bbf2
Nicolas Boichat <nicolas@boichat.ch>
2013-05-28 14:43:55 +0800

    Merge clipboard branch.
    Squashed commit of the following:
    
    commit 43b5f02974d3e9e62fbfc0975e474d861e726fee
    Author: Nicolas Boichat <nicolas@boichat.ch>
    Date:   Sat May 25 22:39:40 2013 +0800
    
        Quiet down croutonclip on startup.
    
    commit e955ffd1ff699cad8833374b01fa60540885572f
    Author: Nicolas Boichat <nicolas@boichat.ch>
    Date:   Thu May 16 19:46:35 2013 +0800
    
        Create packaged app (crx file), that is easier to install.
    
    commit e2d757901f17011057209e049ae82da60e7c475d
    Author: Nicolas Boichat <nicolas@boichat.ch>
    Date:   Tue May 14 19:56:49 2013 +0800
    
        Make sure that clipboard content is copied over on chroot X server exit.
    
    commit 0085c93168ce9b76af30d248f749785011a3b548
    Author: Nicolas Boichat <nicolas@boichat.ch>
    Date:   Sun May 12 18:26:32 2013 +0800
    
        Clearer and more correct variable names in croutonclip.
    
    commit b670fa69f8d33d9acb1cee66b9ae82da2b027279
    Author: Nicolas Boichat <nicolas@boichat.ch>
    Date:   Sat May 11 15:38:13 2013 +0800
    
        Install xclip and netcat-openbsd in xephyr target.
    
    commit f59228a2e5cf79ad177b1aab490b1d210024d5a3
    Author: Nicolas Boichat <nicolas@boichat.ch>
    Date:   Sat May 11 15:09:39 2013 +0800
    
        Install croutonclip in xephyr target.
    
    commit 9c7d69cf5bbf7eecdfcc982bf7ca8f255a5ed397
    Author: Nicolas Boichat <nicolas@boichat.ch>
    Date:   Sat May 11 15:08:54 2013 +0800
    
        croutonclip Chromium OS app: Fix listener.
    
    commit 1193681d3c04aeba890e3e99901943827ae271f3
    Author: Nicolas Boichat <nicolas@boichat.ch>
    Date:   Sat May 11 15:05:21 2013 +0800
    
        croutonclip: - Avoid returning non-zero exit status, as it makes
        ratpoison display a warning, and causes artifacts.
        - Check nc command status when copying the clipboard, so that we do not
        erase the chroot clipboard if we do not have any valid content from
        Chromium OS.
    
    commit 53873096fa19c3435ca067e139d76b8c76a73e03
    Author: Nicolas Boichat <nicolas@boichat.ch>
    Date:   Sat May 11 14:27:27 2013 +0800
    
        Improved croutonclip. Still a few issues to be sorted out (netcat
        and possible race condition)...
    
    commit 66d5f254d7b8dfdbb45b0c137b1ad295381aef56
    Author: Nicolas Boichat <nicolas@boichat.ch>
    Date:   Sat May 11 14:18:43 2013 +0800
    
        Gracefully destroy socket if the window is closed.
    
    commit c81f47b40fe6b0cb9a8e758c38996e68d2d2572e
    Author: Nicolas Boichat <nicolas@boichat.ch>
    Date:   Sat May 11 13:48:55 2013 +0800
    
        Improved Chrome OS application: cleaned up code, more sanity checks.
    
    commit 8557209a01446ced8e4e76bce77e1fffa047c7ca
    Author: Nicolas Boichat <nicolas@boichat.ch>
    Date:   Thu May 9 23:58:35 2013 +0800
    
        croutonclip guest OS service to handle clipboard transfers.
    
    commit 3e717a07548db1ab3d472a2c9275ec8ae3877ae2
    Author: Nicolas Boichat <nicolas@boichat.ch>
    Date:   Thu May 9 22:20:30 2013 +0800
    
        Simple JS-based server to synchronize clipboard between Chrome and crouton.

---
+++ host-app/croutonclip/background.js
@@ -0,0 +1,176 @@
+var DEBUG = false;
+var ADDRESS = "127.0.0.1";
+var PORT = 30001;
+
+var appwin_; // Application window
+var socketId_; // Listen socket Id
+var acceptId_; // Accept socket Id
+var reading_ = false; // Currently reading a second block of data
+
+var document_;
+var clipboardholder_;
+var logger_;
+var info_;
+
+function clipboardStart() {
+    console.log("start!");
+
+    chrome.app.window.create('window.html', {
+        'id': 'croutonclip',
+        'bounds': {
+            'width': 400,
+            'height': 300
+        },
+        'hidden': !DEBUG
+        },
+        function(win) {
+            appwin_ = win;
+            // See http://code.google.com/p/chromium/issues/detail?id=148522
+            win.contentWindow.addEventListener("load", appWindowCallback);
+        }
+    );
+}
+
+function appWindowCallback() {
+    document_ = appwin_.contentWindow.document;
+    clipboardholder_ = document_.getElementById("clipboardholder");
+    logger_ = document_.getElementById("logger");
+    info_ = document_.getElementById("info");
+
+    printDebug("appWindowCallback");
+
+    chrome.socket.create('tcp', {}, function(createInfo) {
+        socketId_ = createInfo.socketId;
+        chrome.socket.listen(socketId_, ADDRESS, PORT, function(result) {
+            printDebug("listen (" + result + ")");
+            if (result < 0) {
+                printError("Cannot listen on " + ADDRESS + ":" + PORT +
+                    ": Is the port already in use by another application?");
+                return;
+            }
+            chrome.socket.accept(socketId_, onSocketAccept);
+        });
+    });
+
+    appwin_.onClosed.addListener(clipboardStop);
+}
+
+function onSocketAccept(acceptInfo) {
+    acceptId_ = acceptInfo.socketId;
+    printDebug("accept");
+    if (acceptInfo.resultCode < 0) {
+        printError("Error on accept.");
+        // FIXME: Try to accept a new connection? Or bail out?
+        return;
+    }
+
+    readNext(false);
+}
+
+function readNext(reading) {
+    reading_ = reading;
+    chrome.socket.read(acceptId_, onSocketRead);
+}
+
+function onSocketRead(readInfo) {
+    printDebug("read(" + readInfo.resultCode + ")");
+
+    if (readInfo.resultCode < 0) {
+        if (reading_) {
+            printDebug("copy (" + clipboardholder_.value + ")");
+            reading_ = false;
+            clipboardholder_.style.display = "block";
+            clipboardholder_.select();
+            document_.execCommand("Copy");
+            clipboardholder_.style.display = "none";
+            acceptNext();
+            return;
+        }
+    }
+
+    var data = arrayBufferToString(readInfo.data, 0);
+
+    printDebug("read(data=" + data + ")");
+
+    if (reading_ || (data.length > 0 && data[0] == 'P')) {
+        // Copy data to clipboard
+        clipboardholder_.style.display = "block";
+        if (!reading_) {
+            reading_ = true;
+            clipboardholder_.value = data.substring(1);
+        } else {
+            clipboardholder_.value += data;
+        }
+        readNext(true);
+    } else {
+        clipboardholder_.style.display = "block";
+        clipboardholder_.value = "";
+        clipboardholder_.select();
+        document_.execCommand("Paste");
+        clipboardholder_.style.display = "none";
+        var clipdata = clipboardholder_.value;
+        var clipdataarray = stringToUint8Array(clipdata);
+        var outputBuffer = new ArrayBuffer(clipdataarray.byteLength);
+        var view = new Uint8Array(outputBuffer)
+        view.set(clipdataarray, 0);
+
+        printDebug("clip(" + clipdata + ")");
+        chrome.socket.write(acceptId_, outputBuffer, onSocketWrite);
+    }
+}
+
+function onSocketWrite(writeInfo) {
+    // FIXME: Check that all the bytes have been written?
+    printDebug("write(" + writeInfo.bytesWritten + ")");
+    acceptNext();
+}
+
+function acceptNext() {
+    chrome.socket.destroy(acceptId_);
+    chrome.socket.accept(socketId_, onSocketAccept);
+}
+
+function clipboardStop() {
+    printDebug("clipboardStop");
+    chrome.socket.destroy(socketId_);
+}
+
+// Copied from chrome-app-samples/webserver
+function stringToUint8Array(string) {
+    var buffer = new ArrayBuffer(string.length);
+    var view = new Uint8Array(buffer);
+    for(var i = 0; i < string.length; i++) {
+      view[i] = string.charCodeAt(i);
+    }
+    return view;
+};
+
+// Copied from chrome-app-samples/webserver
+function arrayBufferToString(buffer, index) {
+    var str = '';
+    var uArrayVal = new Uint8Array(buffer);
+    for(var s = index; s < uArrayVal.length; s++) {
+        str += String.fromCharCode(uArrayVal[s]);
+    }
+    return str;
+};
+
+function printDebug(str) {
+    console.log(str);
+    if (DEBUG) {
+        logger_.textContent += str + "\n";
+    }
+}
+
+function printError(str) {
+    var info = appwin_.contentWindow.document.getElementById("info");
+    info.textContent = str;
+    /* Make sure the window gets visible */
+    appwin_.show();
+    appwin_.drawAttention();
+}
+
+chrome.app.runtime.onLaunched.addListener(function() {
+    clipboardStart();
+});
+
